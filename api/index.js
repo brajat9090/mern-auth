e10ca736c451af99ccf68670a01480d5ba471397
Rajat Bhardwaj <brajat9090@gmail.com>
2024-03-09 21:54:20 +0530

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server Listening on port 3000');
-})
+  console.log("Server Listening on port 3000");
+});
