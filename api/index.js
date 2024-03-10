92f662b2a71100e77e58e9d3bf408b2307e5242d
Rajat Bhardwaj <brajat9090@gmail.com>
2024-03-10 20:16:58 +0530

    Add pre prod changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
+
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -17,11 +19,19 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server Listening on port 3000");
